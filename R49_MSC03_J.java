91a1f04cbbb11ebbed39f9179fee0e31efe14d9d
Alejandro <Alejandroortega599@gmail.com>
2025-10-05 23:58:24 -0400

    Update R49_MSC03_J.java

--- R49_MSC03_J.java
+++ R49_MSC03_J.java
@@ -1,3 +1,7 @@
 public class R49_MSC03_J {
-  public static void main(String[] args){ String password = "P@ssw0rd!"; System.out.println(password.length()); }
+  public static void main(String[] args){
+    String password = System.getenv("APP_PASSWORD");
+    if (password == null) throw new IllegalStateException("set APP_PASSWORD");
+    System.out.println(password.length());
+  }
 }
