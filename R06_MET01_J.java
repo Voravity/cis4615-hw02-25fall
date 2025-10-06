933024825ffca348a1d77fe1ddc85c92135a5dbe
Alejandro <Alejandroortega599@gmail.com>
2025-10-05 23:54:33 -0400

    Update R06_MET01_J.java

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,10 +1,9 @@
 public class R06_MET01_J {
   static int getAbsAdd(int x, int y){
-    assert x != Integer.MIN_VALUE;
-    assert y != Integer.MIN_VALUE;
+    if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) throw new IllegalArgumentException();
     int ax = Math.abs(x);
     int ay = Math.abs(y);
-    assert ax <= Integer.MAX_VALUE - ay;
+    if (ax > Integer.MAX_VALUE - ay) throw new IllegalArgumentException();
     return ax + ay;
   }
   public static void main(String[] args){ System.out.println(getAbsAdd(1,2)); }
