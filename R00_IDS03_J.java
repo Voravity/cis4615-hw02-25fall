adddf881b7e2a0f8956e76229efd69a15fdcfde6
Alejandro <Alejandroortega599@gmail.com>
2025-10-05 23:47:58 -0400

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,15 +1,17 @@
 import java.util.logging.*;
 import java.util.*;
+import java.util.regex.Pattern;
 public class R00_IDS03_J {
-  public static void main(String[] args) {
+  static String sanitizeUser(String u){ return Pattern.matches("[A-Za-z0-9 ]+", u) ? u : "unauthorized user"; }
+  public static void main(String[] args){
     Logger logger = Logger.getLogger("app");
     Scanner sc = new Scanner(System.in);
     String username = sc.nextLine();
     boolean loginSuccessful = username.equals("admin");
     if (loginSuccessful) {
-      logger.severe("User login succeeded for: " + username);
+      logger.severe("User login succeeded for: " + sanitizeUser(username));
     } else {
-      logger.severe("User login failed for: " + username);
+      logger.severe("User login failed for: " + sanitizeUser(username));
     }
   }
 }
