bf61ffa4e39619f08255aae5e6f45141d45ab732
Alejandro <Alejandroortega599@gmail.com>
2025-10-05 23:49:19 -0400

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -2,8 +2,8 @@ import java.io.*;
 public class R02_XP00_J {
   static void deleteFile() throws IOException {
     File f = new File("someFileName.txt");
-    f.createNewFile();
-    f.delete();
+    if (!f.exists()) f.createNewFile();
+    if (!f.delete()) throw new IOException("delete failed");
   }
   public static void main(String[] args) throws Exception { deleteFile(); }
 }
