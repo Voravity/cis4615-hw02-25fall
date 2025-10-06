8faec617e0d62477c680a70a963c29384b0cb4fd
Alejandro <Alejandroortega599@gmail.com>
2025-10-05 23:57:54 -0400

    Update R13_FIO14_J.java

--- R13_FIO14_J.java
+++ R13_FIO14_J.java
@@ -2,6 +2,7 @@ import java.io.*;
 public class R13_FIO14_J {
   public static void main(String[] args) throws Exception {
     File f = File.createTempFile("demo",".tmp");
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> { if (f.exists()) f.delete(); }));
     try (FileWriter w = new FileWriter(f)) { w.write("data"); }
   }
 }
