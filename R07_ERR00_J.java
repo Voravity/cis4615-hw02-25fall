187f233e02aff8d56a515734196a8213a444477e
Alejandro <Alejandroortega599@gmail.com>
2025-10-05 23:55:32 -0400

    Update R07_ERR00_J.java

--- R07_ERR00_J.java
+++ R07_ERR00_J.java
@@ -2,6 +2,6 @@ import java.io.*;
 public class R07_ERR00_J {
   public static void main(String[] args){
     try (BufferedReader r = new BufferedReader(new FileReader("missing.txt"))) { r.readLine(); }
-    catch (IOException e) { }
+    catch (IOException e) { throw new RuntimeException(e); }
   }
 }
