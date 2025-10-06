6e7ec414896bb2571af610c3ea9dbb7a0174f762
Alejandro <Alejandroortega599@gmail.com>
2025-10-05 23:52:20 -0400

    Update R04_STR03_J.java

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -2,10 +2,10 @@ import java.math.BigInteger;
 public class R04_STR03_J {
   public static void main(String[] args){
     BigInteger x = new BigInteger("530500452766");
-    byte[] b = x.toByteArray();
-    String s = new String(b);
-    b = s.getBytes();
-    x = new BigInteger(b);
+    String s = x.toString();
+    byte[] b = s.getBytes();
+    String ns = new String(b);
+    x = new BigInteger(ns);
     System.out.println(x);
   }
 }
